8fa5b1d08148b668cb16063eca003b23ff240ba1
adammck <adam.mckaig@gmail.com>
2012-05-15 13:10:14 -0400

    Refactor the README generator.
    No change in functionality.

--- lib/forkreadme/generator.rb
+++ lib/forkreadme/generator.rb
@@ -8,56 +8,83 @@ module ForkReadme
   class Generator
     include URI::REGEXP::PATTERN
 
-    def initialize dir
-      @dir = dir
+    def initialize path, with_images
+      @path = path
+      @with_images = with_images
+
+      repo_name = github_repo_name @path
+      @repo = octokit.repo repo_name
+      @parent = parent_of @repo
     end
 
     def readme
-      repo_name = github_repo_name @dir
-
-      if repo_name.empty?
-        raise NotRepo.new "Not a GitHub repo: #{@dir}"
-      end
-
-      repo = octokit.repo repo_name
-
-      unless repo.fork
-        raise NotFork.new "Not a GitHub fork: #{repo_name}"
-      end
-
-      parent_name = full_name repo.parent
-      parent = octokit.repo parent_name
-
-      s = []
-      s << "This is a fork of [#{parent.name}] (#{parent.html_url}), with pull requests:"
-      s << ""
-
-      logins = collaborator_logins repo
-      my_pulls = pull_requests(parent).select do |pull|
-        pull.user and logins.include?(pull.user.login)
-      end
-
-      my_pulls.each do |short_pull|
-        pull = pull_request(parent, short_pull.number)
-        if repo_name == full_name(pull.head.repo)
-          s << " * [#{pull.title}] (#{pull.html_url})"
-        end
-      end
-
-      s.join "\n"
+      intro + "\n\n" + (links.join "\n")
     end
-  
-  
+
+    # Public: Returns the introduction paragraph.
+    def intro
+      link = link_to(@parent.name, @parent.html_url)
+      "This is a fork of #{link}, with pull requests:"
+    end
+
+    # Public: Returns a paragraph summarizing the pull requests.
+    def links
+      pull_requests(@parent).select do |pull|
+        pull.head.repo.id == @repo.id
+
+      end.map do |pull|
+        "* " + line_for(pull, @with_images)
+      end
+    end
+
     private
-  
-    # Private: Returns an Array containing the logins of all collaborators for an
-    # Octokit repo.
+
+    # Private: Returns a line of Markdown summarizing a pull request.
+    def line_for pull_request, with_images
+      img = if with_images
+        image "Status of ##{pull_request.number}", status_image(pull_request)
+      end
+
+      suffix = img ? (" " + img) : ""
+      link_to(pull_request.title, pull_request.html_url) + suffix
+    end
+
+    # Private: Returns the parent repo (as an Octokit repo) of an Octokit repo,
+    # or raises NotFork if the repo does not have a parent.
+    def parent_of child_repo
+      if child_repo.parent
+        parent_repo_name = full_name child_repo.parent
+        octokit.repo parent_repo_name
+      else
+        child_name = full_name child_repo
+        raise NotFork.new "Not a GitHub fork: #{child_name}"
+      end
+    end
+
+    # Private: Returns a link in Markdown format.
+    def link_to text, href
+      "[#{text}] (#{href})"
+    end
+
+    # Private: Returns an image in Markdown format.
+    def image alt_text, href
+      "!" + link_to(alt_text, href)
+    end
+
+    # Private: Returns an Array containing the logins of all collaborators for
+    # an Octokit repo.
     def collaborator_logins repo
       octokit.collabs(full_name repo).map do |user|
         user.login
       end
     end
-  
+
+    # Private: Returns the URL of the status image (via pullstat.us) for an
+    # Octokit pull request.
+    def status_image pull_request
+      pull_request.html_url.sub "github.com", "pullstat.us"
+    end
+
     # Private: Returns all pull request summaries (as returned by the "List pull
     # requests" API, and wrapped by Octokit) for an Octokit repo.
     def pull_requests repo
@@ -68,16 +95,17 @@ module ForkReadme
       end
     end
   
-    # Private: Return full pull request (as returned by the "Get a single pull
-    # request" API, and wrapped by Octokit) to an Octokit repo and pull number.
-    def pull_request repo, number
-      octokit.pull full_name(repo), number
-    end
-  
-    # Private: Returns the full GitHub repo name of a Git working directory.
+    # Private: Returns the full GitHub repo name (e.g. adammck/forkreadme) of a
+    # Git working directory, or raises NotRepo.
     def github_repo_name path
       clone_url = parse_url remote_url path
-      chop_extension clone_url.path
+
+      if clone_url.host.downcase == "github.com"
+        chop_extension chop_leading_slash clone_url.path
+
+      else
+        raise NotRepo.new "Not a GitHub repo: #{path}"
+      end
     end
   
     # Private: Returns the full GitHub repo name of an Octokit repo.
@@ -103,17 +131,22 @@ module ForkReadme
   
       rescue URI::InvalidURIError
         if m = url.match("^(#{USERINFO})@(#{HOST}):(#{REL_PATH})$")
-          URI::Generic.new "ssh", m[1], m[2], 22, nil, m[3], nil, nil, nil
+          URI::Generic.new "ssh", m[1], m[2], 22, nil, "/" + m[3], nil, nil, nil
   
         else
           raise
         end
       end
     end
-  
+
     # Private: Returns a filename with the extension removed.
     def chop_extension filename
       filename.sub /\.\w+$/, ""
+    end
+
+    # Private: Returns a filename with the leading slash removed.
+    def chop_leading_slash filename
+      filename.sub /^\//, ""
     end
   end
 end
